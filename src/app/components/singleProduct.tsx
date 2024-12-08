3170bbd3ebaec544465e56fdb945307fda409919
ahmer990 <ahmerabbasi990@gmail.com>
2024-12-09 00:05:58 +0500

    first commit

---
+++ src/app/components/singleProduct.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const SingleProduct = () => {
+  return (
+    <div>
+
+
+<div>ahmer is a good boy</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default SingleProduct
---
+++ src/app/page.tsx
@@ -0,0 +1,31 @@
+
+import Home from "./components/home";
+// import Footer from "./components/footer";
+import Shop from "./Shop/shop";
+import Contact from "../../Conatct/contact";
+import SingleProduct from "./components/singleProduct";
+
+
+export default function Hoome() {
+  return (
+
+    <><div>
+
+
+      <Home />
+
+
+
+    </div>
+        <div>
+
+{/* < Shop /> */}
+
+{/* < footer /> */}
+        </div>
+{/* < Contact /> */}
+
+{/* <SingleProduct /> */}
+      </>
+  );
+}
